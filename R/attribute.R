4445a6e99ed7414b23e5c8c10d8b5404ec3e2b51
JonathanYe3 <jonathanye2003@gmail.com>
2021-03-16 16:00:58 -0400

    Demo attribute function, returns a large list of physiological attribute data.frames

---
+++ R/attribute.R
@@ -0,0 +1,25 @@
+#' Fetch physiological attribute data
+#'
+#' @param keyword a character vector of attribute names desired. The attributes available are:
+#' all, gram, oxygen, size, arrangement, spore, shape
+#'
+#' @return a large list of tidy data.frames
+#' @export
+#'
+#' @examples
+#' x <- attribute()
+#' head(x[[1]])
+#'
+#' y <- attribute(c("gram", "oxygen"))
+#' head(y[[1]])
+#'
+attribute <- function(keyword = "all"){
+  url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTDk8wow4yN_IrqbltZP-6w4rwf0JCNRiPL9jWWSqvTu4da6AgxJNAED98r-rSJeFE1msqsBpzPlk4a/pub?output=csv"
+  links <- read.csv(url)
+  links <- links[links$Physiology %in% keyword,]
+
+  sheets <- as.list(links[[2]])
+  dat <- lapply(sheets, read.csv)
+  names(dat) <- links[[1]]
+  dat
+}
