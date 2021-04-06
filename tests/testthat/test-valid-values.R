655311ab54e36b0bf1544bab456175011bdca706
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-04-06 16:44:49 -0500

    updated unit test for attribute values.

--- tests/testthat/test-valid-values.R
+++ tests/testthat/test-valid-values.R
@@ -1,7 +1,18 @@
 
 database <- attribute()
 
-test_that("headers are valid.", {
+is_attrval_c <- function(x) {
+  for (i in seq_along(x)) {
+    if ( names(x[i]) == "Attribute_value" &&
+         is.character(x[[i]])
+         # !is.numeric(x[[i]]) &&
+         # !is.double(x[[i]])
+    )
+      return(x[[i]])
+  }
+}
+
+test_that("all headers are present and valid.", {
 
   x <- lapply(database, function(x) names(x))
   actual_headers <- sort(unique(unlist(x, use.names = FALSE)))
@@ -21,9 +32,10 @@ test_that("headers are valid.", {
 
   })
 
-test_that("attribute values are valid.", {
+test_that("all categorical attribute values are present and valid.", {
 
-  y <- lapply(database, function(x) x[["Attribute_value"]])
+  y <- lapply(database, function(x) is_attrval_c(x))
+  y <- y[-which(sapply(y, is.null))]
   actual_values <- sort(unique(unlist(y, use.names = FALSE)))
   filename <- system.file("extdata", "attributevalues.tsv",
                           package = "bugphyzz")
