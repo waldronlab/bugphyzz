4d5028df0a275690df1a53d0d315db8f3887b179
lwaldron <lwaldron.research@gmail.com>
2021-04-09 14:18:44 -0400

    rename attributes -> physiologies

--- R/physiologies_list.R
+++ R/physiologies_list.R
@@ -1,12 +1,10 @@
-#' Fetch list of available physiological attribute names
+#' Fetch list of available physiologies
 #'
-#' @return a character vector of physiological attribute names
+#' @return a character vector of physiologies
 #' @export
 #'
 #' @examples
-#' x <- attribute_list()
-attribute_list <- function(){
-  url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTDk8wow4yN_IrqbltZP-6w4rwf0JCNRiPL9jWWSqvTu4da6AgxJNAED98r-rSJeFE1msqsBpzPlk4a/pub?output=csv"
-  dat <- read.csv(url)
-  dat[[1]]
+#' x <- physiologies_list()
+physiologies_list <- function(){
+  curationLinks()[["physiology"]]
 }
