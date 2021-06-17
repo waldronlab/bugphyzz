da51c4549fa5123b234fdaad6d9708c26127e5cc
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-06-17 18:44:53 -0500

    Update the documentation of the fattyAcidComposition and customLinks functions.

--- R/fattyAcidComposition.R
+++ R/fattyAcidComposition.R
@@ -1,22 +1,21 @@
-#' function for shaping fatty acid composition from Google Sheets using tidyR
-#' @param keyword a character vector of custom links desired. For the available
-#' custom links, run bugphyzz::custom_link(). Use "all" for all available custom links.
+utils::globalVariables(c("functionname", "Br-C10:1", "Oxo-C19:1"))
+#' Function for shaping fatty acid composition from Google Sheets using tidyR
 #'
-#' @return a tidy data frame of fattyacidComposition from customlinks.tsv
+#' @return a tidy data frame of fattyAcidComposition from customlinks.tsv
 #'
 #' @importFrom  tidyr pivot_longer
+#' @importFrom magrittr %>%
 #'
 #' @export
 #'
 #' @examples
+#' library(bugphyzz)
 #' fattyAcidComposition()
 #' x <- fattyAcidComposition()
-#' View(x)
+#' dplyr::glimpse(x)
 #'
-
 #load fatty acid composition sheet from customlinks.tsv
 #test to check whether custom link exists
-
 fattyAcidComposition <- function(){
   link <- customLinks() %>%
     dplyr::filter(functionname == "fattyAcidComposition") %>%
@@ -30,6 +29,22 @@ fattyAcidComposition <- function(){
   return(fac_long)
 }
 
+#' Custom Links
+#'
+#' \code{customLinks} returns a data frame with links to custom data sets not included
+#' in the \code{physiologies} function.
+#'
+#' @param keyword a character vector of custom links desired.
+#' Use "all" for all available custom links.
+#'
+#' @return
+#' A data frame with custom links.
+#'
+#' @export
+#'
+#' @examples
+#' library(bugphyzz)
+#' customLinks(keyword = "all")
 customLinks <- function(keyword = "all"){
   fname <-
     system.file("extdata/customlinks.tsv", package = "bugphyzz")
@@ -38,6 +53,3 @@ customLinks <- function(keyword = "all"){
            links[links$physiology %in% keyword,])
   return(links)
 }
-
-
-
