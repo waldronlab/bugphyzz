3dedda7d6d183300a3633b2daf3d424065d8eba9
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-06-19 21:17:22 -0500

    Changed expectValidHeaders functions to checkRequiredColumns funcionts. All check functions are now in the check_functions.R file. Custom error (stop) functions were added to the stop_functions.R file. A new file (template.tsv) and new functions (checkColumnValues) were added to test valid values accross columns.

--- tests/testthat/test-valid-values.R
+++ tests/testthat/test-valid-values.R
@@ -1,31 +1,25 @@
-
-database <- physiologies()
-
-is_attr_c <- function(x) {
-  for (i in seq_along(x)) {
-    if ( names(x[i]) == "Attribute" && is.character(x[[i]]) ) {
-      return(x[[i]])
-    }
-  }
-}
-
-test_that("all headers are present and valid in each data set", {
-            for (i in seq_along(database)) {
-              expectValidHeaders(database[[i]], names(database[i]))
-              }
-  })
-
-test_that("all categorical attribute values are present and valid.", {
-
-  y <- lapply(database, function(x) is_attr_c(x))
-  y <- y[!sapply(y, is.null)]
-  actual_values <- sort(unique(unlist(y, use.names = FALSE)))
-
-  filename <- system.file("extdata", "attributes.tsv",
-                          package = "bugphyzz")
-  df <- read.table(filename, sep = "\t", header = TRUE)
-  valid_values <- sort(unique(df[["attribute"]]))
-
-  expect_identical(actual_values, valid_values)
-
-})
+#
+# database <- physiologies()
+#
+# is_attr_c <- function(x) {
+#   for (i in seq_along(x)) {
+#     if ( names(x[i]) == "Attribute" && is.character(x[[i]]) ) {
+#       return(x[[i]])
+#     }
+#   }
+# }
+#
+# test_that("all categorical attribute values are present and valid.", {
+#
+#   y <- lapply(database, function(x) is_attr_c(x))
+#   y <- y[!sapply(y, is.null)]
+#   actual_values <- sort(unique(unlist(y, use.names = FALSE)))
+#
+#   filename <- system.file("extdata", "attributes.tsv",
+#                           package = "bugphyzz")
+#   df <- read.table(filename, sep = "\t", header = TRUE)
+#   valid_values <- sort(unique(df[["attribute"]]))
+#
+#   expect_identical(actual_values, valid_values)
+#
+# })
