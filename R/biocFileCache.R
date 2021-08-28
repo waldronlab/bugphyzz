9a98643224c2740184083513068429963b2df5de
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-08-28 18:08:41 -0500

    getSignatures functionality

--- R/biocFileCache.R
+++ R/biocFileCache.R
@@ -9,6 +9,7 @@
   cache_path <- tools::R_user_dir(package = "bugphyzz", which = "cache")
   cache <- BiocFileCache::BiocFileCache(cache = cache_path, ask = FALSE)
   return(cache)
+
 }
 
 #' Create taxonomy table
@@ -21,7 +22,7 @@
 #'
 #' Two versions of the taxonomy table are saved in a list object (.RData),
 #' one version with taxonomy names and another with NCBI IDs. See the
-#' \code{\link{taxonomyTable}} function to know how to access each of them.
+#' \code{\link{.taxonomyTable}} function to know how to access each of them.
 #'
 #' @return A taxonomy table (*.RData object) saved in the cache, and a message
 #' indicating that the taxonomy table has been saved to the cache.
@@ -36,26 +37,13 @@
 #'
 .createTaxonomyTable <- function() {
 
-  # define function to modify the output of taxize::classification
-  # into a tibble.
-  # A similar result can be achieved with cbind. Consider using cbind
-  # instead of reshape_taxon_table
-  reshape_taxon_table <- function(x, type) {
-    if (tail(x[["rank"]], 1) == "no rank") {
-      x$rank[length(x[["rank"]])] <- "no_rank"
-    }
-    x <- x[!x[["rank"]] %in% c("no rank", "clade"), ]
-    tibble::as_tibble(t(x[type]), .name_repair = "minimal") %>%
-      magrittr::set_colnames(gsub(" ", "_", x[["rank"]]))
-  }
-
   suppressMessages({
     datasets <- physiologies()
     datasets[["fatty acid composition"]] <- fattyAcidComposition()
   })
 
   suppressWarnings({
-    # There could be some warning of NAs introdcued by coercion when
+    # There could be some warning of NAs introduced by coercion when
     # converting to integer
     ncbi_ids <- datasets %>%
       # as.integer helps to remove invalid NCBI IDs
@@ -73,41 +61,33 @@
 
   # This step is necessary because invalid IDs might cause that outputs
   # become misplaced with respect to the vector names
-  new_names <- map_chr(taxonomies, ~tail(.x[["id"]], 1))
+  new_names <- purrr::map_chr(taxonomies, ~tail(.x[["id"]], 1))
   names(taxonomies) <- new_names
 
   ranks <- taxonomies %>%
     purrr::map_chr(~tail(.x$rank, 1))
   ranks_table <- tibble::tibble(NCBI_ID = names(ranks), rank = ranks)
 
-  taxonomy_table_names <- taxonomies %>%
-    purrr::map(~ reshape_taxon_table(.x, type = "name")) %>% # function defined above
-    dplyr::bind_rows(.id = "NCBI_ID") %>%
-    dplyr::distinct() # remove duplicates
+  taxonomy_table <- cbind(taxonomies) %>%
+    dplyr::rename(NCBI_ID = query) %>%
+    dplyr::left_join(ranks_table, by = "NCBI_ID") %>%
+    dplyr::relocate("NCBI_ID", "rank") %>%
+    dplyr::distinct()
 
-  taxonomy_table_ids <- taxonomies %>%
-    purrr::map(~ reshape_taxon_table(.x, type = "id")) %>% # function defined above
-    dplyr::bind_rows(.id = "NCBI_ID") %>%
-    dplyr::distinct() # remove duplicates
-
-  taxonomy_table_with_ranks_names <- dplyr::left_join(ranks_table, taxonomy_table_names, by = "NCBI_ID")
-  taxonomy_table_with_ranks_ids <- dplyr::left_join(ranks_table, taxonomy_table_ids, by = "NCBI_ID")
-
-  taxonomy_tables <- list(name = taxonomy_table_with_ranks_names, id = taxonomy_table_with_ranks_ids)
-
-  # The following steps are for saving to cache
   cache <- .getCache()
   cache_info <- BiocFileCache::bfcinfo(cache)
   rids <- cache_info %>%
-    dplyr::filter(rname == "taxonomy_tables") %>%
+    dplyr::filter(rname == "taxonomy_table") %>%
     dplyr::pull(rid)
   if (length(rids) > 0) {
     for (i in seq_along(rids)) {
       BiocFileCache::bfcremove(cache, rids[i])
     }
   }
-  save_path <- BiocFileCache::bfcnew(cache, "taxonomy_tables", ext=".RData")
-  save(taxonomy_tables, file = save_path)
+
+  save_path <- BiocFileCache::bfcnew(cache, "taxonomy_table", ext = ".tsv")
+  # readr::write_tsv(taxonomy_table, file = save_path)
+  write.table(x = taxonomy_table, file = save_path, quote = TRUE, sep = "\t", na = "NA")
 
   message("A new taxonomy table has been saved to cache.")
 
@@ -115,7 +95,7 @@
 
 #' Taxonomy table
 #'
-#' \code{taxonomyTable} imports a taxonomy table of all of the NCBI IDs
+#' \code{.taxonomyTable} imports a taxonomy table of all of the NCBI IDs
 #' across BugPhyzz.
 #'
 #' When the function is used for the first time, a taxonomy table is created
@@ -147,36 +127,36 @@
 #'
 #' @return A tibble.
 #'
-#' @export
+#' @keywords internal
 #'
 #' @examples
 #'
 #' \dontrun{
 #' library(bugphyzz)
-#' taxonomy_table <- taxonomyTable(type = "name", update = FALSE)
+#' taxonomy_table <- bugphyzz:::.taxonomyTable(type = "name", update = FALSE)
 #' taxonomy_table
 #' }
 #'
-taxonomyTable <- function(type = "name", update = FALSE) {
+.taxonomyTable <- function(update = FALSE) {
 
   cache <- .getCache()
 
-  rid <- BiocFileCache::bfcquery(cache, query = "taxonomy_tables", field = "rname") %>%
+  rid <- BiocFileCache::bfcquery(cache, query = "taxonomy_table", field = "rname", exact = TRUE) %>%
     dplyr::pull(rid)
 
   if (!length(rid)) {
     cat("There is no taxonomyTable saved in cache. Creating one. This might take a few minutes.", sep = "\n")
     .createTaxonomyTable()
-    rid <- BiocFileCache::bfcquery(cache, query = "taxonomy_tables", field = "rname") %>%
+    rid <- BiocFileCache::bfcquery(cache, query = "taxonomy_table", field = "rname", exact = TRUE) %>%
       dplyr::pull(rid)
   } else if (isTRUE(update)) {
     cat("Update parameter was set to TRUE. Creating a new taxonomyTable. This might take a few minutes.", sep = "\n")
     .createTaxonomyTable()
-    rid <- BiocFileCache::bfcquery(cache, query = "taxonomy_tables", field = "rname") %>%
+    rid <- BiocFileCache::bfcquery(cache, query = "taxonomy_table", field = "rname", exact = TRUE) %>%
       dplyr::pull(rid)
   }
 
   rpath <- BiocFileCache::bfcrpath(cache, rids = rid)
-  taxonomy_tables <- get(load(rpath))
-  taxonomy_tables[[type]]
+  # readr::read_tsv(rpath, show_col_types = FALSE)
+  read.table(rpath, header = TRUE, sep = "\t")
 }
--- R/makeSignatures.R
+++ R/makeSignatures.R
@@ -1,8 +1,15 @@
 #' Makes taxid signatures for physiologies
 #'
+#' \code{makeSignatures}  reates a list of microbe signatures from a bugphyzz
+#' dataset.
+#'
 #' @param df A `data.frame` output by the \link{physiologies} function
 #' @param taxids identification of the bugs (Taxon name, NCBI ID, etc.)
-#' @param rank Nothing yet
+#' @param taxa_rank Nothing yet
+#' @param min An integer. Minimum value. Only applies for attributes with
+#' continuous values.
+#' @param max An integer. Maximum value. Only applies for attributes with
+#' continoues values.
 #'
 #' @return a large list of character vectors
 #' @export
@@ -14,11 +21,59 @@
 #' x <- makeSignatures(aero[[1]], taxids = "NCBI_ID")
 #' lapply(x, head)
 
-makeSignatures <- function(df, taxids = "Taxon_name", rank = "all"){
+makeSignatures <- function(df, taxids = "Taxon_name", taxa_rank = "genus", min = NULL, max = NULL) {
+
+  valid_taxids <- c("NCBI_ID", "Genome_ID", "Accession_number", "Taxon_name")
+
+  if (!taxids %in% valid_taxids)
+    stop("Invalid taxid type. Valid options:", paste0(valid_taxids, collapse = "|"), call. = FALSE)
+
+  valid_ranks <- c("genus", "species", "strain", "all")
+
+  if (!taxa_rank %in% valid_ranks)
+    stop("Invalid rank name. Valid options:", paste0(valid_taxids, collapse = "|"), call. = FALSE)
+
+  taxonomy_table <- .taxonomyTable()
+
+  if (!is.null(min) || !is.null(max)) {
+
+    if (is.null(min))
+      min <- min(df[["Attribute_value"]])
+    if (is.null(max))
+      max <- max(df[["Attribute_value"]])
+
+    df <- df %>%
+      dplyr::filter(Attribute_value >= min, Attribute_value <= max)
+
+  } else if (is.null(min) && is.null(max)) {
+
+    df <- df %>%
+      dplyr::filter(Attribute_value == TRUE)
+
+  }
+
+  if (taxa_rank == "all") {
+
+    df <- dplyr::left_join(df, taxonomy_table, by = "NCBI_ID") %>%
+      dplyr::mutate(rank = ifelse(!rank %in% c("genus", "species", "strain"), "strain", rank))
+
+  } else {
+
+    df <- dplyr::left_join(df, taxonomy_table, by = "NCBI_ID") %>%
+      dplyr::filter(rank == taxa_rank) %>%
+      dplyr::mutate(rank = ifelse(!rank %in% c("genus", "species", "strain"), "strain", rank))
+
+  }
+
   attribute_names <- unique(df[["Attribute"]])
   dat <- lapply(attribute_names, fetchBugs, dat = df, taxids = taxids)
   names(dat) <- attribute_names
-  return(dat)
+
+  if (!length(dat)) {
+    NULL
+  } else {
+    dat
+  }
 }
 
 #' Fetch bugs based on taxid and corresponding attribute
@@ -40,4 +95,110 @@ fetchBugs <- function(attribute_name, dat, taxids = "Taxon_name"){
   bugs <- unique(bugs[[taxids]])
   return(bugs[!is.na(bugs)])
 }
+
+#' Get signatures
+#'
+#' \code{getSignatures} creates a list of microbe signatures from a bugphyzz
+#' dataset. The signatures are created from inherited ranks.
+#'
+#' @param df A dataset from bugphyzz.
+#' @param rank A character string with the taxonomy rank of the signatures.
+#' Options: "superkingdom", "kingdom", "phylum", "class", "order", "family",
+#' "species", "strain".
+#' @param id_type A character string with the type of ID. Options: "Taxon_name", "NCBI_ID", "Accession_number", "Genome_ID".
+#' @param min An integer. Minimum value. Only applies for attributes with
+#' continuous values.
+#' @param max An integer. Maximum value. Only applies for attributes with
+#' continoues values.
+#'
+#' @return
+#' A list of microbe signatures
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(bugphyzz)
+#'
+#' # Make signatures of bacteria classified by oxygen utilization
+#'
+#' aer <- physiologies("aerophilicity")[[1]]
+#' aer_signatures <- getSignatures(df = aer, rank = "genus", id_type = "name")
+#'
+#' # Make signature of bacteria growing in extreme temperatures
+#'
+#' gt <- physiologies("growth temperature")[[1]]
+#' gt_signatures <- getSignatures(df = gt, rank = "species", id_type = "name", min = 80)
+#'
+getSignatures <- function(df, rank = "genus", id_type = "name", min = NULL, max = NULL) {
+
+  valid_ranks <- c("superkingdom", "phylum", "class", "order", "family", "genus", "species", "strain")
+  valid_id_types <- c("Taxon_name", "NCBI_ID", "Accession_number", "Genome_ID")
+
+  if (!rank %in% valid_ranks)
+    stop(paste0("Invalid 'rank'. Valid options: ", paste0(valid_ranks, collapse = ", "), "."), call. = FALSE)
+
+  if (!id_type %in% valid_id_types)
+    stop(paste0("Invalid 'id_type'. Valid options: ", paste0(valid_id_types, collapse = ", "), "."), call. = FALSE)
+
+  rank_id <- paste0(rank, "_id")
+  taxonomy_table <- .taxonomyTable()
+
+  if (!is.null(min) || !is.null(max)) {
+
+    if (is.null(min))
+      min <- min(df[["Attribute_value"]])
+    if (is.null(max))
+      max <- max(df[["Attribute_value"]])
+
+    df <- df %>%
+      dplyr::filter(Attribute_value >= min, Attribute_value <= max)
+
+  } else if (is.null(min) && is.null(max)) {
+
+    df <- df %>%
+      dplyr::filter(Attribute_value == TRUE)
+
+  }
+
+  if (id_type == "Taxon_name") {
+    # name is actually obtained from rank, not the Taxon_name column.
+    # This is because of possible duplicates (synonyms).
+    signatures <- rank
+    signatures_names <- rank_id
+    select_cols <- c(rank, rank_id, "Attribute")
+  } else if (id_type == "NCBI_ID") {
+    # name is actually obtained from rank id, not the NCBI_ID column.
+    # This is because of possible duplicates (synonyms).
+    signatures <- rank_id
+    signatures_names <- rank
+    select_cols <- c(rank_id, rank, "Attribute")
+  } else if (id_type == "Genome_ID" || id_type == "Accession_number") {
+    signatures <- id_type
+    signatures_names <- rank_id
+    select_cols <- c(id_type, rank_id, "Attribute")
+  }
+
+  data <- df %>%
+    dplyr::left_join(taxonomy_table, by = "NCBI_ID") %>%
+    dplyr::mutate(strain = ifelse(!rank %in% c("genus", "species", "strain"), Taxon_name, strain),
+                  strain_id = ifelse(!rank %in% c("genus", "species", "strain"), NCBI_ID, strain_id)) %>%
+    dplyr::select(tidyselect::all_of(select_cols)) %>%
+    dplyr::distinct() %>%
+    tidyr::drop_na()
+
+  output <- split(data, data[["Attribute"]]) %>%
+    lapply(function(x) {
+      taxa <- x[[signatures]]
+      names(taxa) <- x[[signatures_names]]
+      taxa
+    })
+
+  if (!length(output)) {
+    NULL
+  } else {
+    output
+  }
+}
+
 
