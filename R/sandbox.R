87e0a5c94aeeee4fe3d9236df265abc216d38a9f
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-10-20 21:06:43 -0500

    Remove sandbox file, no longer useful.

--- R/sandbox.R
+++
@@ -1,6 +0,0 @@
-reqCols <- function() {
-    fname <- system.file("extdata/template.tsv", package = "bugphyzz")
-    df <- utils::read.table(fname, sep = "\t", header = TRUE)
-    df <- df[df[["requiredness"]] == "required",]
-    df[order(df[["required_column_order"]]), "column_name"]
-}
