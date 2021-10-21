9b4f981e7ba6f50774e0265d9567c8ab3a1a673e
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-10-20 21:13:55 -0500

    The bergeysAbstracts function is useful only for curatos, so it should not be exported. Add @keyword internal in roxygen2 field.

--- R/bergeysAbstracts.R
+++ R/bergeysAbstracts.R
@@ -1,13 +1,16 @@
 #' Fetch Bergeys abstracts
-#' This function fetches Kelly Eckenrode's and Jonathan Ye's scraping of the Bergey's abstracts and returns a tidy data.frame
+#'
+#' This function fetches Kelly Eckenrode's and Jonathan Ye's scraping of the
+#' Bergey's abstracts and returns a tidy data.frame
+#'
 #' @return A tidy data.frame
-#' @export
+#' @keywords internal
 #'
 #' @examples
 #' x <- bergeysAbstracts()
 #' dim(x)
 bergeysAbstracts <- function(){
-  dat <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRfDmQEmkl0QxwQ8A-DfrPmrRu6jQRwMfkz3eBOjJQsvKOTfXD-nkcmF6kIhFWjI1i92xWeraZghK1F/pub?gid=1045368624&single=true&output=csv",
-                  check.names = FALSE, stringsAsFactors = FALSE)
+  fname <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vRfDmQEmkl0QxwQ8A-DfrPmrRu6jQRwMfkz3eBOjJQsvKOTfXD-nkcmF6kIhFWjI1i92xWeraZghK1F/pub?gid=1045368624&single=true&output=csv"
+  dat <- utils::read.csv(fname, check.names = FALSE, stringsAsFactors = FALSE)
   return(dat)
 }
