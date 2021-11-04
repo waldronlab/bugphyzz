7ffac8afbe51ee95ad0460192270c8cfdce0d6d3
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-11-04 13:22:42 -0600

    Add check_valid_ncbi_ids to unit tests.

--- tests/testthat/test-curation.R
+++ tests/testthat/test-curation.R
@@ -2,6 +2,30 @@
 phys <- physiologies()
 phys[["fatty acid composition"]] <- fattyAcidComposition()
 
+
+## A function for testing for validity of NCBI_IDs
+check_valid_NCBI_IDs <- function(dat) {
+
+  ncbi_ids <- dat[["NCBI_ID"]]
+  taxonomies <- taxizedb::classification(ncbi_ids, db = "ncbi")
+  invalid_positions_lgl <- !is.na(ncbi_ids) & is.na(taxonomies)
+  names(invalid_positions_lgl) <- NULL
+  invalid_positions <- which(invalid_positions_lgl)
+
+  if (!length(invalid_positions)) {
+    message("All NCBI IDs are valid.")
+    return(NULL)
+  }
+
+  invalid_values <- ncbi_ids[invalid_positions]
+
+  list(invalid_positions = invalid_positions, invalid_values = invalid_values)
+
+  .stop_custom("invalid_ncbi_ids", message = "Invalid NCBI IDs.",
+    invalid_positions = invalid_positions, invalid_values = invalid_values)
+}
+
+
 # test_that("all datasets pass curation tests", {
 #     expect_null(bugphyzz:::.checkColumnValuesList(phys))
 #     expect_null(bugphyzz:::.checkRequiredColumnsList(phys))
@@ -22,3 +46,21 @@ test_that("the NCBI_ID column is not of type numeric.", {
 
     expect_length(ncbi_id_class_err, 0)
 })
+
+
+test_that("all NCBI_IDs are valid.", {
+
+  err_list <- lapply(phys, function(x) {
+    tryCatch(
+      invalid_ncbi_ids = function(e) e,
+      check_valid_NCBI_IDs(x)
+    )
+  }) %>%
+    discard(is.null)
+  expect_length(err_list, 0)
+})
+
+
+
+
+
