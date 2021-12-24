c7dbcc938be7fcdf798075032f1bf6f1a5676706
Samuel Gamboa <Samuel.Gamboa.Tuz@gmail.com>
2021-12-23 22:24:32 -0600

    Update makeSignatures

--- R/makeSignatures.R
+++ R/makeSignatures.R
@@ -1,138 +1,91 @@
-utils::globalVariables(c("NCBI_ID", "Attribute", "rank", ".", "bugphyzz_rank"))
 #' Make signatures
 #'
-#' \code{makeSignatures} creates a list of microbe signatures from a bugphyzz
-#' dataset, i.e., a dataset imported either with the \code{\link{physiologies}}
-#' or the \code{\link{fattyAcidComposition}} functions.
+#' \code{makeSignatures} converts a bugphyzz dataset into a list of microbial
+#' signatures.
 #'
-#' @section Types of signatures:
+#' @param df A data.frame imported with bugphyzz.
+#' @param tax_id_type Type of ID. Options: NCBI_ID, Taxon_name.
+#' @param tax_level Taxonomy level. Options: superkingdom, kingom, phylum,
+#' class, order, family, genus, species, strain, mixed (all taxonomies).
+#' Default is mixed.
+#' @param evidence Type of evidence of the assigned attribute values to a
+#' given taxon. Options: EXP, experimental; ASR, ancestral state reconstruction.
+#' Default is both EXP and ASR.
+#' @param ci A double indicating how likely is that a given
+#' organism has that attribute value. Threshold options: 1, always; 0.8,
+#' Usually; 0.5, sometimes; 0.3, Occasionally.
+#' Values below 0.3 are not allowed. Default is 0.8.
+#' @param include_unknown_ci Include taxa whose attribute value confidence
+#' interval is unknown.
+#' @param min_size The minimal size of the returned microbial signatures.
+#' Default is 1.
 #'
-#' \code{\link{makeSignatures}} allows the creation of three types of
-#' signatures (regular, ranked, or inherited) with the `type` argument.
-#'
-#' + The `regular` option (default) creates a list of signatures per attribute
-#' considering all of the taxids and attributes in the dataset, regardless of
-#' taxonomic rank.
-#'
-#' + The `ranked` option filters the taxids by taxonomic rank before creating
-#' the lists of signatures. Currently, only "species", "genus", and "strain"
-#' rank levels are supported.
-#'
-#' + The `inherited` option allows the creation of signatures in which the
-#' members of the list inherit an attribute annotation from a taxon of a lower
-#' level, as long as they belong to the same lineage. For example, if the
-#' *Pyrococcus abyssi* species is not annotated in a given bugphyzz dataset,
-#' but the *Pyrococcus furiosus* DSM 3638 strain is, then the
-#' `type = "inherited"` argument allows to create a signature at the species
-#' level including the *Pyrococcus abyssi* species.
-#'
-#' @param df A bugphyzz dataset.
-#' @param taxids A character string indicating the type of taxid. Valid
-#' options: NCBI_ID, Taxon_name, Genome_ID, Accession_ID.
-#' @param tax_rank A character string indicating the taxonomic rank of the
-#' microbes in the signatures. Valid options: superkingdom, kingdom, phylum,
-#' class, order, family, species, strain.
-#' @param min_sig_size The minimum number of microbes in a signature.
-#' @param sig_type A character string. Valid options: regular, normal, inherited.
-#'
-#' @return
-#' A named list of microbe signatures.
+#' @return A list of microbial signatures
 #'
 #' @export
 #'
 #' @examples
 #'
-#' library(bugphyzz)
-#' library(dplyr)
-#'
-#' ## Example 1- Crete a list of signatures from the aerophilicity dataset
 #' aer <- physiologies("aerophilicity")[[1]]
-#' aer_sig <- makeSignatures(aer, taxids = "NCBI_ID", min_sig_size = 5)
+#' aer_signatures <- makeSignatures(aer)
+#' lapply(aer, head)
 #'
-#' ## Exampl 2 - Create signatures of thermophilic bacteria
-#' gt <- physiologies("growth temperature")[[1]]
-#' tm <- gt |> filter(Attribute_value > 80)
-#'
-#' ## Species level signatures with ranked option
-#' tm_sig_ranked <- makeSignatures(tm, taxids = "Taxon_name",
-#'                                 tax_rank = "species", sig_type = "ranked")
-#'
-#' ## Species level signtures with inherited option
-#' tm_sig_inherited <- makeSignatures(tm, taxids = "Taxon_name",
-#'                                    tax_rank = "species",
-#'                                    sig_type = "inherited")
-#'
-#' ## Compare lengths
-#' length(tm_sig_inherited[[1]]) > length(tm_sig_ranked[[1]])
-#'
-makeSignatures <- function(df, taxids, sig_type = "regular", tax_rank = NULL, min_sig_size = 1) {
+makeSignatures <- function(
+  df, tax_id_type = "NCBI_ID", tax_level = "mixed",
+  evidence = c("EXP", "ASR", "Unknown"), ci = 0.8,
+  include_unknown_ci = FALSE, min_size = 5
+) {
 
-  valid_ranks <- c("superkingdom", "kingdom", "phylum", "class", "order",
-                   "family", "genus", "species", "strain")
+  valid_ranks <- c(
+    "superkingdom", "phylum", "class", "order", "family", "genus",
+    "species", "strain"
+  )
 
-  valid_taxids <- c("NCBI_ID", "Taxon_name", "Genome_ID", "Accession_ID")
-
-  valid_sig_types <- c("regular", "ranked", "inherited")
-
-  if (!taxids %in% valid_taxids)
-    stop("Invalid taxid. Select one of: ", paste0(valid_taxids, collapse = ", "), ".", call. = FALSE)
-
-  if (!sig_type %in% valid_sig_types)
-    stop("Invalid signature type. Select one of: ", paste0(valid_sig_types, collapse = ", "), ".", call. = FALSE)
-
-  annotated_df <- df %>%
-    dplyr::mutate(NCBI_ID = as.character(as.integer(NCBI_ID))) %>%
-    suppressWarnings() %>%
-    dplyr::left_join(taxonomyAnnotations, by = c("NCBI_ID" = "bugphyzz_NCBI_ID")) %>%
-    dplyr::filter(Attribute != FALSE, !is.na(Attribute))
-
-  if (sig_type == "inherited") {
-
-    if (is.null(tax_rank))
-      stop("Taxonomic rank not specified. You need to specify one with the `tax_rank` argument to create 'ranked' or 'inherired' signatures.", call. = FALSE)
-
-    if (!is.null(tax_rank) && !tax_rank %in% valid_ranks)
-      stop("Invalid taxonomy rank. Select one of: ", paste0(valid_ranks, collapse = ", "), ".", call. = FALSE)
-
-    if (taxids == "Taxon_name") {
-      taxids <- paste0("bugphyzz_", tax_rank)
-    } else if (taxids == "NCBI_ID") {
-      taxids <- paste0("bugphyzz_", tax_rank, "_id")
-    } else {
-      stop("Argument taxids can only take 'NCBI_ID' or 'Taxon_name' for inherited signatures.", call. = FALSE)
-    }
-
-  } else if (sig_type == "ranked") {
-
-    if (is.null(tax_rank))
-      stop("Taxonomic rank not specified. You need to specify one with the `tax_rank` argument to create 'ranked' or 'inherired' signatures.", call. = FALSE)
-
-    if (!is.null(tax_rank) && !tax_rank %in% c("genus", "species", "strain"))
-      stop("Invalid taxonomy rank with 'sig_type = \"ranked\"' option.",
-           " Select one of: genus, species, or strain", call. = FALSE)
-
-    annotated_df <- annotated_df %>%
-      dplyr::filter(bugphyzz_rank == tax_rank)
-
-  } else if (sig_type == "regular") {
-
-    if (!is.null(tax_rank))
-      warning("Taxonomic rank not required for 'regular' signatures. `tax_rank` argument value (", tax_rank, ") ignored.", call. = FALSE)
+  if (tax_level == "mixed") {
+    tax_level <- valid_ranks
   }
 
-  signatures <- annotated_df %>%
-    dplyr::select(tidyselect::all_of(c(taxids, "Attribute"))) %>%
-    dplyr::distinct() %>%
-    tidyr::drop_na() %>%
-    split(f = as.factor(.[["Attribute"]])) %>%
-    purrr::map(~ unique(.x[[taxids]])) %>%
-    purrr::discard(~ length(.x) < min_sig_size)
+  ci_vals <- c(always = 1, usually = 0.8, sometimes = 0.5, occasionally = 0.3)
 
-  if (!length(signatures)) {
-    message("No signatures.")
-    return(NULL)
+  if (ci == ci_vals[1]) {
+    filter_ci <- names(ci_vals)[1]
+
+  } else if (ci >= ci_vals[2]) {
+    filter_ci <- names(ci_vals)[1:2]
+
+  } else if (ci >= ci_vals[3]) {
+    filter_ci <- names(ci_vals)[1:3]
+
+  } else if (ci >= ci_vals[4]) {
+    filter_ci <- names(ci_vals)[1:4]
   }
 
-  signatures
+  if (include_unknown_ci)
+    filter_ci <- c(filter_ci, "Unknown")
 
+  filter_rows <- df$Rank %in% tax_level &
+    df$Evidence %in% evidence &
+    df$Confidence_interval %in% filter_ci
+
+  select_cols <- c(tax_id_type, "Attribute")
+
+  output <- df[filter_rows, select_cols]
+
+  output <- split(output, factor(output[["Attribute"]]))
+
+  output <- lapply(output, function(x) {
+    values <- x[[1]]
+    names <- x[[2]]
+    names(values) <- names
+    unique(values)
+  })
+
+  output <-
+    output[vapply(output, function(x)  length(x) >= min_size, logical(1))]
+
+  if (!length(output)) {
+    message("No signatures found.")
+    return(invisible(NULL))
+  }
+  return(output)
 }
