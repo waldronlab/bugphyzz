d3c26f530f302cbe5fb89e3db0e0f18e9d9e7084
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-03-31 17:09:58 -0400

    modified script

--- data-raw/protraits_raw.R
+++ data-raw/protraits_raw.R
@@ -12,37 +12,6 @@ suppressMessages({
   library(googlesheets4)
 })
 
-## A helper function ####
-## This function is for formatting the output of taxize/taxizedb
-taxize_classification_to_taxonomy_table <- function(tax, id_type = "name") {
-  valid_ranks <- c(
-    "superkingdom", "class", "order", "family", "genus", "species"
-  )
-  query_names <- as.data.frame(names(tax))
-  colnames(query_names) <- "query"
-  taxonomy_list <- lapply(tax, function(x) {
-    if (any(is.na(x))) {
-      data.frame(
-        kingdom = NA, class = NA, order = NA, family = NA,
-        genus = NA, species = NA
-      )
-    } else {
-      df <- x[x$rank %in% valid_ranks, ]
-      df <- df[,c("rank", id_type)]
-      df <- as.data.frame(t(df))
-      col_names <- as.character(df[1,])
-      df <- as.data.frame(df[-1,])
-      colnames(df) <- col_names
-      colnames(df)[colnames(df) == "superkingdom"] <- "kingdom"
-      rownames(df) <- NULL
-      df
-    }
-  })
-  taxonomy_table <- taxonomy_list %>%
-    dplyr::bind_rows()
-  cbind(query_names, taxonomy_table)
-}
-
 ## Download and import data ####
 
 rname <- "bugphyzz_protraits"
