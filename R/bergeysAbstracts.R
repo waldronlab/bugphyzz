24c4b35756b39e894824ae04d76b496338ab4b80
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-12-06 18:26:33 -0500

    Remove vignette/article about the .berbeysAbstracts function. Document with roxygen instead.

--- R/bergeysAbstracts.R
+++ R/bergeysAbstracts.R
@@ -1,14 +1,15 @@
 #' Fetch Bergeys abstracts
 #'
-#' This function fetches Kelly Eckenrode's and Jonathan Ye's scraping of the
-#' Bergey's abstracts and returns a tidy data.frame
+#' \code{.bergeyAbstracts} fetches Kelly Eckenrode's and Jonathan Ye's scraping
+#' of the Bergey's abstracts and returns a tidy data.frame.
 #'
-#' @return A tidy data.frame
+#' @return A tidy data.frame.
+#'
 #' @keywords internal
 #'
 #' @examples
-#' x <- bugphyzz:::.bergeysAbstracts()
-#' dim(x)
+#' bergeys <- bugphyzz:::.bergeysAbstracts()
+#' head(bergeys)
 .bergeysAbstracts <- function(){
   fname <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vRfDmQEmkl0QxwQ8A-DfrPmrRu6jQRwMfkz3eBOjJQsvKOTfXD-nkcmF6kIhFWjI1i92xWeraZghK1F/pub?gid=1045368624&single=true&output=csv"
   dat <- utils::read.csv(fname, check.names = FALSE, stringsAsFactors = FALSE)
