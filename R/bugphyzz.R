d79511b972228aae50d768caa3d99129fa627180
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-03-07 17:11:57 -0500

    clean empty lines

--- R/bugphyzz.R
+++ R/bugphyzz.R
@@ -1,10 +1,8 @@
 
 importBugphyzz <- function(keyword = 'all', version = 'devel', cache = TRUE) {
-
   if (version == 'devel') {
     file <- 'https://raw.githubusercontent.com/waldronlab/bugphyzzExports/main/full_dump.csv'
   }
-
   bugphyzz <- vroom::vroom(
     file, delim = ',', skip = 1, show_col_types = FALSE,
     col_types = vroom::cols(
@@ -19,8 +17,6 @@ getBugphyzzSignatures <- function(
     df, tax.id.type = c('ncbi', 'taxname'), tax.level = 'mixed', min.size = 5,
     evidence = c('asr', 'inh', 'exp', 'tas', 'nas', 'igc'),
     frequency = c('unknown', 'always', 'usually', 'sometimes')
-
     # exact.tax.level = TRUE, min.size = 5
 ) {
-
 }
