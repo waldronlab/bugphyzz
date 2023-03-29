e79eb34d1bf8e252042d16260da3c06a6cfbf519
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-03-29 02:04:15 -0400

    update get signatures

--- R/bugphyzz.R
+++ R/bugphyzz.R
@@ -32,19 +32,62 @@ importBugphyzz <- function(keyword = 'all', version = 'devel', cache = TRUE) {
   )
   if ('all' %in% keyword) {
     output <- split(bp, factor(bp$Attribute_group))
+    output <- purrr::map(output, ~ purrr::discard(.x, ~all(is.na(.x))))
     return(output)
   }
   bp <- bp |>
     dplyr::filter(.data$Attribute %in% keyword)
   output <- split_df(bp, factor(bp$Attribute_group))
+  output <- purrr::map(output, ~ purrr::discard(.x, ~all(is.na(.x))))
   return(output)
 }
 
+#' Get Bugphyzz Signatures
+#'
+#' \code{getBugphyzzSignatures} convert data to signatures
+#'
+#' @param df A data.frame
+#' @param tax.id.type NCBI_ID or Taxon_name.
+#' @param tax.level Domain to strain.
+#' @param evidence asr, inh, exp, igc, nas, tas
+#' @param frequency unknown, sometimes, usually, always
+#' @param min.size Default 5.
+#'
+#' @return List of signatures
+#' @export
+#'
+#' @examples
+#'
+#' library(purrr)
+#' bp <- importBugphyzz()
+#' my_fun <- function(x) {
+#'   getBugphyzzSignatures(
+#'     df = x, tax.id.type = 'Taxon_name', tax.level = 'species', min.size = 5,
+#'     frequency = 'always'
+#'   )
+#' }
+#' o <- flatten(map(bp, my_fun))
+#'
+#'
 getBugphyzzSignatures <- function(
-    df, tax.id.type = c('ncbi', 'taxname'), tax.level = 'mixed',
+    df, tax.id.type = 'NCBI_ID', tax.level = 'mixed',
     evidence = c('asr', 'inh', 'exp', 'tas', 'nas', 'igc'),
     frequency = c('unknown', 'always', 'usually', 'sometimes'),
     min.size = 5
 ) {
-  ## TODO
+  valid_ranks <- c(
+    'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species',
+    'strain'
+  )
+  if (tax.level == 'mixed') {
+    tax.level = valid_ranks
+  }
+  df <- df[which(df$Rank %in% tax.level),]
+  df <- df[which(df$Evidence %in% evidence), ]
+  df <- df[which(df$Frequency %in% frequency),]
+  output <- split(df, factor(df$Attribute))
+  output <- lapply(output, function(x) unique(x[[tax.id.type]]))
+  output <- purrr::discard(output, ~ length(.x) < min.size)
+  return(output)
+
 }
