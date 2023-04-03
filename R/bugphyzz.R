cb1e3e78da232e7dd17cfe97a252b734f2587613
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-04-03 17:52:48 -0400

    add function importBugphyzzNumeric for importing numeric values. Output is a list of data.frames because every data.frame can have different units

--- R/bugphyzz.R
+++ R/bugphyzz.R
@@ -1,5 +1,5 @@
 
-#' Import bugphyzz as a single data.frame (or several?
+#' Import bugphyzz data as a single data.frame
 #'
 #' \code{importBugphyzz} imports bugphyzz.
 #'
@@ -19,11 +19,10 @@ importBugphyzz <- function(keyword = 'all', version = 'devel', cache = TRUE) {
     ## TODO add code for BiocFileCache
   }
   if (version == 'devel') {
-    # url <- 'https://github.com/waldronlab/bugphyzzExports/blob/sdgamboa/update-exports/full_dump_bugphyzz.csv.bz2?raw=true'
-    url <- 'https://github.com/waldronlab/bugphyzzExports/raw/sdgamboa/update-exports/full_dump_bugphyzz.csv.bz2'
+    url <- 'https://github.com/waldronlab/bugphyzzExports/raw/sdgamboa/update-exports/full_dump_categorical.csv.bz2'
   }
   temp_dir <- tempdir()
-  temp_file <- paste0(temp_dir, '/', 'full_dump_bugphyzz.csv.bz2')
+  temp_file <- paste0(temp_dir, '/', 'full_dump_categorical.csv.bz2')
   download.file(url = url, destfile = temp_file, quiet = TRUE)
   ## Need to add skip = 1 to the vroom call when header is added to the text
   bp <- vroom::vroom(
@@ -41,6 +40,54 @@ importBugphyzz <- function(keyword = 'all', version = 'devel', cache = TRUE) {
   # output <- purrr::map(output, ~ purrr::discard(.x, ~all(is.na(.x))))
   return(bp)
 }
+
+#' Import bugphyzz data as a list of numeric data.frames
+#'
+#' \code{importBugphyzzNumeric} imports numeric attributes as a list of
+#' data.frames. Attributes are in separate data.frames since they can
+#' have different units for filtering (e.g. Celsius degrees, ph, etc.)
+#'
+#' @param keyword Name of the numeric attributes.
+#' Use \code{showBugphyzzNumeric} to check valid values.
+#' Default value is 'all'.
+#' @param version Zenodo version of dataset. If 'devel' the data from
+#' the bugphyzzExports repo on GitHub will be downloaded.
+#' @param cache Whether use or not cached data. Default is TRUE.
+#'
+#' @return A list of data.frames.
+#' @export
+#'
+#' @examples
+#'
+#' numeric <- importBugphyzzNumeric(version = 'devel', cache = TRUE)
+#'
+importBugphyzzNumeric <- function(keyword = 'all', version = 'devel', cache = TRUE) {
+  if (cache) {
+    ## TODO
+  }
+  if (version == 'devel') {
+    url <- 'https://github.com/waldronlab/bugphyzzExports/raw/sdgamboa/update-exports/full_dump_numeric.csv.bz2'
+  }
+  temp_dir <- tempdir()
+  temp_file <- paste0(temp_dir, '/', 'full_dump_numeric.csv.bz2')
+  download.file(url = url, destfile = temp_file, quiet = TRUE)
+  ## Need to add skip = 1 to the vroom call when header is added to the text
+  bp <- vroom::vroom(
+    file = temp_file, show_col_types = FALSE, delim = ',', progress = FALSE,
+    col_types = vroom::cols(NCBI_ID = vroom::col_character())
+  )
+  if ('all' %in% keyword) {
+    output <- split(bp, factor(bp$Attribute_group))
+    output <- purrr::map(output, ~ purrr::discard(.x, ~all(is.na(.x))))
+    return(output)
+  }
+  bp <- bp |>
+    dplyr::filter(.data$Attribute %in% keyword)
+  output <- split_df(bp, factor(bp$Attribute_group))
+  output <- purrr::map(output, ~ purrr::discard(.x, ~all(is.na(.x))))
+  return(output)
+}
+
 
 #' Get Bugphyzz Signatures
 #'
--- inst/scripts/example.R
+++ inst/scripts/example.R
@@ -1,8 +1,12 @@
-library(bugphyzz)
-##
+# library(bugphyzz)
 bp <- importBugphyzz(keyword = 'all', version = 'devel', cache = FALSE)
+num <- importBugphyzzNumeric(keyword = 'all', version = 'devel', cache = FALSE)
+
 sigs <- getBugphyzzSignatures(
   bp, tax.id.type = 'Taxon_name', tax.level = 'order', frequency = 'always'
 )
-lapply(sigs, head)
+
+
+
+
 
