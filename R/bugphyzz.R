749c38cff5acecaa3efc2b47501f36d9af4bf069
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-07-19 14:57:21 -0400

    update getBugphyzzSignatures function

--- R/bugphyzz.R
+++ R/bugphyzz.R
@@ -164,25 +164,59 @@ importBugphyzzNumeric <- function(
 getBugphyzzSignatures <- function(
     df, tax.id.type = 'NCBI_ID', tax.level = 'mixed',
     evidence = c('asr', 'inh', 'exp', 'tas', 'nas', 'igc'),
-    frequency = c('unknown', 'always', 'usually', 'sometimes'),
+    frequency = c('unknown', 'rarely', 'always', 'usually', 'sometimes'),
     min.size = 5
 ) {
-  valid_ranks <- c(
-    'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species',
-    'strain'
-  )
+  valid_ranks <- validRanks()
+
   if (tax.level == 'mixed') {
     tax.level <- valid_ranks
   }
   df <- df[which(df$Rank %in% tax.level),]
   df <- df[which(df$Evidence %in% evidence), ]
   df <- df[which(df$Frequency %in% frequency),]
-  output <- split(df, factor(df$Attribute))
-  output <- lapply(output, function(x) unique(x[[tax.id.type]]))
-  output <- purrr::discard(output, ~ length(.x) < min.size)
-  return(output)
+  df$Attribute <- paste0(df$Attribute_group,'|', df$Attribute)
+  df <- df |>
+    dplyr::mutate(
+      Attribute_range = ifelse(
+        test = is.na(Attribute_range),
+        yes = 'REMOVETHIS',
+        no = Attribute_range)
+    ) |>
+    dplyr::mutate(
+      Attribute = sub(
+        ' REMOVETHIS$', '', paste0(Attribute, ' ', Attribute_range)
+      )
+    )
+  dfs <- split(df, factor(df$Attribute))
+  dfs <- lapply(dfs, function(x) unique(x[, c(tax.id.type, 'Rank')]))
+  return(dfs)
+  dfs <- purrr::discard(dfs, ~ nrow(.x) < min.size)
+  sig_ranks <- purrr::map(dfs, ~ {
+    v <- unique(.x$Rank)
+    v <- factor(
+      x = v, levels = c('domain', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'strain'),
+      ordered = TRUE
+    )
+    v <- sort(v)
+    v <- as.character(v)
+    dplyr::case_when(
+      v == 'domain' ~ 'd',
+      v == 'phylum' ~ 'p',
+      v == 'class' ~ 'c',
+      v == 'order' ~ 'o',
+      v == 'family' ~ 'f',
+      v == 'genus' ~ 'g',
+      v == 'species' ~ 's',
+      v == 'strain' ~ 't',
+      TRUE ~ v
+    )
+  })
+  sig_ranks <- purrr::map_chr(sig_ranks, ~ paste0(.x, collapse = ''))
+  sigs <- purrr::map(dfs, ~ unique(.x[[tax.id.type]]))
+  names(sigs) <- paste0('bp:', names(sigs), '|', sig_ranks)
+  return(sigs)
 }
-
 
 #' Get Bug Annotations
 #'
@@ -278,7 +312,7 @@ taxRanks <- function() {
 #'
 validRanks <- function() {
   c(
-    "superkingdom", "phylum", "class", "order", "family", "genus",
+    "domain", "phylum", "class", "order", "family", "genus",
     "species", "strain"
   )
 }
